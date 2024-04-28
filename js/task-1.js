8ab912fe1c44ecbd56604f5afc8a2ee59902897d
coopperhead <121683884+coopperhead@users.noreply.github.com>
2024-04-28 18:26:14 +0200

    wrote the functions
    wrote the functions

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
+
+// Перевірка роботи функції
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
